df112c1d1082ac3fd5d289cca7eea56e6b9beca4
Đồng Nguyễn <dongbuh@gmail.com>
2020-09-23 23:04:53 +0700

    update

--- routes/admin.php
+++ routes/admin.php
@@ -6,5 +6,4 @@ Route::prefix(config('core.admin_prefix'))->middleware(['web', 'admin.auth'])->g
     Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard.index');
     Route::get('dashboard/setting',  [DashboardController::class, 'setting'])->name('admin.dashboard.setting');
     Route::post('dashboard/save', [DashboardController::class, 'save'])->name('admin.dashboard.save');
-
 });
